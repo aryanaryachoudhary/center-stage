87dc33a6f5ccfa15eeb5dbbb598f3b639b079ca8
shouryashashank <shouryashashank@gmail.com>
2022-05-15 11:09:53 +0530

    added obs support

--- __init__.py
+++ __init__.py
@@ -1,26 +1,33 @@
+import pyvirtualcam
 import cv2
 
-cap = cv2.VideoCapture(0)   #(id of primary webcam)
+cap = cv2.VideoCapture(0)
 # cap.set(3,640)      #(id of width,dimension)
 # cap.set(4,480)      #(id of height,dimension)
 # cap.set(10,100)     #(if for brightness, brightness level)
-print(cap)
 
-while True:
-    success, img = cap.read()
-    face_cascade = cv2.CascadeClassifier("resources/face.xml")
-    # body_cascade = cv2.CascadeClassifier("resources/body.xml")
+fmt = pyvirtualcam.PixelFormat.BGR
+with pyvirtualcam.Camera(width=1280, height=720, fps=20, fmt = fmt) as cam:
+    while True:
+        ret_val, frame = cap.read()
+        face_cascade = cv2.CascadeClassifier("resources/face.xml")
+        # body_cascade = cv2.CascadeClassifier("resources/body.xml")
 
-    imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        imgGray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(imgGray,1.1,4)
+        # bodies = body_cascade.detectMultiScale(imgGray,1.1,4)
 
-    faces = face_cascade.detectMultiScale(imgGray,1.1,4)
-    # bodies = body_cascade.detectMultiScale(imgGray,1.1,4)
+        for(x,y,w,h) in faces:
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+        # for(x,y,w,h) in bodies:
+        #     cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
 
-    for(x,y,w,h) in faces:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-    
-    # for(x,y,w,h) in bodies:
-    #     cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-    cv2.imshow("FaceCam",img)
-    if cv2.waitKey(1) & 0xFF ==ord('q'):
-        break
+
+        frame = cv2.resize(frame, (1280, 720), interpolation=cv2.BORDER_DEFAULT)
+
+        cam.send(frame)
+        cam.sleep_until_next_frame()
+        if cv2.waitKey(1) == 27:
+            break  
+        
+    cv2.destroyAllWindows()
